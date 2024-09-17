81b59502bd5878877c182260498f2fceea7c8162
EC2 Default User <ec2-user@ip-172-31-43-178.us-west-2.compute.internal>
2024-09-17 21:28:56 +0000

    simplified App.js and removed App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload (test).
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>System Check Dashboard</h1>
+      {/* You'll add your charts and quiz components here */}
     </div>
   );
 }
