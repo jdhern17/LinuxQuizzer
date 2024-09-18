cf6b472f6ec03be62f3ac74cf6e85bd5f43ff358
EC2 Default User <ec2-user@ip-172-31-41-98.us-west-2.compute.internal>
2024-09-18 22:28:58 +0000

    updated apolloClient to point to backend:4000 instead of localhost

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', // This is where your backend is running
+  uri: 'http://backend:4000/graphql', // This is where your backend is running
   cache: new InMemoryCache(),
 });
 
