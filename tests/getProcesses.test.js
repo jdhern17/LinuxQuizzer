e191cae6f6fedb51658d4160d88397f7a2a478f6
EC2 Default User <ec2-user@ip-172-31-33-231.us-west-2.compute.internal>
2024-10-14 22:15:26 +0000

    updating tests file, using localhost so no dotenv right now, moving to container-specific URLs

--- tests/getProcesses.test.js
+++ tests/getProcesses.test.js
@@ -1,18 +1,18 @@
-require('dotenv').config(); // Load environment variables
+// require('dotenv').config(); // Load environment variables
 
 const request = require('supertest');
-const baseUrl = `http://${process.env.EC2_PRIVATE_IP}:3001`; // Use the private IP from the .env file
 
-describe('GET /get-processes', () => {
+describe('dummy container GET /get-processes', () => {
+  const dummyBaseUrl = `http://localhost:3001`; // Use the private IP from the .env file
   it('should return a 200 status and a list of processes', async () => {
-    const response = await request(baseUrl).get('/get-processes');
+    const response = await request(dummyBaseUrl).get('/get-processes');
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('processes');
     expect(Array.isArray(response.body.processes)).toBe(true);
   });
 
   it('should return a non-empty processes array', async () => {
-    const response = await request(baseUrl).get('/get-processes');
+    const response = await request(dummyBaseUrl).get('/get-processes');
     expect(response.body.processes.length).toBeGreaterThan(0);
   });
 });
