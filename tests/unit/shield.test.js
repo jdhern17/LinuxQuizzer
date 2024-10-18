dae6d233001a8067aa935baa03f3fa404f5305dc
EC2 Default User <ec2-user@ip-172-31-33-193.us-west-2.compute.internal>
2024-10-18 23:34:41 +0000

    updated test suite with correctly failing for undefined on getDummyData and internal server error on mutation

--- tests/unit/shield.test.js
+++ tests/unit/shield.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
+const graphQLBaseUrl = `http://localhost:4000/graphql`;
 
 // New test block to verify that mutations are denied by default
 describe('GraphQL Shield: Deny Mutations', () => {
-  
+    
     it('should deny all mutations', async () => {
-      const mutation = `
+        const mutation = `
         mutation {
           addProcess {
             pid
@@ -17,8 +18,30 @@ describe('GraphQL Shield: Deny Mutations', () => {
         .post('/')
         .send({ query: mutation });
   
-      expect(response.status).toBe(400);  // Assuming 400 for failed auth, adjust as needed
-      expect(response.body.errors[0].message).toContain('Not Authorised!'); // Or your specific error message
+      expect(response.status).toBe(200);  // Assuming 200 for failed auth, adjust as needed
+    expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toContain('Not Authorized!'); // Or your specific error message
     });
   
+  });
+
+  describe('GraphQL Shield: Deny getDummyData Query', () => {
+  
+    it('should deny getDummyData query', async () => {
+      const query = `
+        query {
+          getDummyData {
+            message
+            simpleValue
+          }
+        }
+      `;
+      const response = await request(graphQLBaseUrl)
+        .post('/')
+        .send({ query });
+  
+      expect(response.status).toBe(200);  // Assuming 200 for failed auth, adjust as needed
+      expect(response.body.errors).toBeDefined();
+      expect(response.body.errors[0].message).toContain('Not Authorized');  // Or your specific error message
+    });
   });
