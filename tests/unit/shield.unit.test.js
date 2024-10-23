3d530d3985267931002dc8060182f4537322dc1d
EC2 Default User <ec2-user@ip-172-31-41-133.us-west-2.compute.internal>
2024-10-23 17:52:58 +0000

    cleaned up shield unit testing

--- tests/unit/shield.unit.test.js
+++ tests/unit/shield.unit.test.js
@@ -1,31 +1,6 @@
 const request = require('supertest');
 const graphqlBaseUrl = 'http://localhost:4000/graphql';
 
-describe('GraphQL Shield: Allow getProcesses Query', () => {
-    it('should allow the getProcesses query and return a list of processes', async () => {
-        const query = `
-        {
-            getProcesses {
-                pid
-                name
-                cpu
-                memVsz
-                user
-                command
-            }
-        }`;
-
-        const response = await request(graphqlBaseUrl)
-            .post('/')
-            .send({ query });
-
-        expect(response.status).toBe(200); // Assuming a successful request
-        expect(response.body.data).toHaveProperty('getProcesses'); // Ensure the response has 'getProcesses'
-        expect(Array.isArray(response.body.data.getProcesses)).toBe(true); // Ensure it's an array
-        expect(response.body.data.getProcesses.length).toBeGreaterThan(0); // Ensure it's not empty
-    });
-});
-
 describe('GraphQL Shield: Deny getDummyData Query', () => {
   
     it('should deny getDummyData query', async () => {
@@ -40,6 +15,7 @@ describe('GraphQL Shield: Deny getDummyData Query', () => {
       const response = await request(graphqlBaseUrl)
         .post('/')
         .send({ query });
+    // temporary proxy for shield validation, needs more granular assessment
       expect(response.status).toBe(200);  // Assuming 200 for failed auth, adjust as needed
       expect(response.body).toHaveProperty('errors');
       expect(response.body.errors).toBeDefined();
