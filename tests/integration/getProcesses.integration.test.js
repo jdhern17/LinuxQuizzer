af4c531f80b2a3e5372ac8402f93d11c6548fdf4
EC2 Default User <ec2-user@ip-172-31-41-133.us-west-2.compute.internal>
2024-10-23 17:24:37 +0000

    reorganized getProcesses integration test

--- tests/integration/getProcesses.integration.test.js
+++ tests/integration/getProcesses.integration.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 describe('Backend container - GraphQL getProcesses query', () => {
     const graphqlBaseUrl = `http://localhost:4000/graphql`;
   
-    it('should return a 200 status and a list of processes', async () => {
+    it('validating structure of responses', async () => {
       const query = `
         {
           getProcesses {
@@ -22,33 +22,36 @@ describe('Backend container - GraphQL getProcesses query', () => {
         .send({ query });
   
       expect(response.status).toBe(200);
-      expect(response.body.data.getProcesses[0].pid).toBeGreaterThan(0); // Ensure a valid process ID is returned
-      expect(response.body.data.getProcesses[0].name).not.toBeNull();    // Ensure the process name is not null      
-      expect(response.body.data.getProcesses[0].pid).not.toBeNull();
       // expect(response.body.errors).toBeFalsy(); // Could be undefined, null, or an empty array
       expect(response.body.errors).toBeUndefined(); // Ensure no error field exists
       expect(response.body.data).toHaveProperty('getProcesses');
       expect(Array.isArray(response.body.data.getProcesses)).toBe(true);
-    });
-  
-    it('should return a non-empty processes array', async () => {
-      const query = `
-        {
-          getProcesses {
-            pid
-            name
-            cpu
-            memVsz
-            user
-            command
-          }
-        }
-      `;
-  
-      const response = await request(graphqlBaseUrl)
-        .post('/')
-        .send({ query });
-  
       expect(response.body.data.getProcesses.length).toBeGreaterThan(0);
+    });
+    
+    it('field by field testing in response', async () => {
+      const query = `
+      {
+        getProcesses {
+          pid
+          name
+          cpu
+          memVsz
+          user
+          command
+          }
+          }
+          `;
+          
+          const response = await request(graphqlBaseUrl)
+          .post('/')
+          .send({ query });
+          
+      expect(response.body.data.getProcesses[0].pid).toBeGreaterThan(0); // Ensure a valid process ID is returned
+      expect(response.body.data.getProcesses[0].name).not.toBeNull();    // Ensure the process name is not null      
+      expect(response.body.data.getProcesses[0].pid).not.toBeNull();
+      expect(response.body.data.getProcesses[0].memVsz).not.toBeNull(); // Ensure memory info is present
+      expect(response.body.data.getProcesses[0].user).not.toBeNull();  // Ensure user info is present
+      expect(response.body.data.getProcesses[0].command).not.toBeNull();  // Ensure command info is present
     });
   });
