9d28d9922da9fe7471c1e9157ee8c55066742fa9
EC2 Default User <ec2-user@ip-172-31-32-188.us-west-2.compute.internal>
2024-10-25 19:53:46 +0000

    removed dummy query from typedefs and resolvers and restructured test folder

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,7 @@ const permissions = shield({
 
 const typeDefs = gql`
   type Query {
-    getDummyData: DummyData
     getProcesses: [Process]
-  }
-
-  type DummyData {
-    message: String
-    simpleValue: Int
   }
 
   type Process {
@@ -47,24 +41,6 @@ const typeDefs = gql`
   // Resolvers updated to pull from dummy container
   const resolvers = {
     Query: {
-      getDummyData: async () => {
-        try {
-          // Make a request to the dummy container's /dummy endpoint
-          // curl -X POST http://localhost:4000/graphql \
-          // -H "Content-Type: application/json" \
-          // -d '{"query": "{ getDummyData { message simpleValue } }"}'
-          const response = await fetch('http://dummy:3001/dummy');
-          const data = await response.json();
-
-          // Destructure the response data to match GraphQL schema
-          const { message, simpleValue } = data;
-          
-          return { message, simpleValue };
-        } catch (error) {
-          console.error('Error fetching system stats:', error);
-          throw new Error('Failed to fetch system stats');
-        }
-      },
       getProcesses: async () => {
         try {
           const response = await fetch('http://dummy:3001/get-processes');
--- tests/security/reconnaissance.security.test.js
+++ tests/security/reconnaissance.security.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const graphqlBaseUrl = 'http://localhost:4000/graphql';
 
-describe('GraphQL Shield: Deny getDummyData Query', () => {
+describe('malicious actor attempting query to nonexisting type', () => {
   
-    it('should deny getDummyData query', async () => {
+    it('should deny query', async () => {
       const query = `
         query {
           getDummyData {
@@ -15,8 +15,7 @@ describe('GraphQL Shield: Deny getDummyData Query', () => {
       const response = await request(graphqlBaseUrl)
         .post('/')
         .send({ query });
-    // temporary proxy for shield validation, needs more granular assessment
-      expect(response.status).toBe(200);  // Assuming 200 for failed auth, adjust as needed
+      expect(response.status).toBe(400);  // 400 failing at the graphql validation level
       expect(response.body).toHaveProperty('errors');
       expect(response.body.errors).toBeDefined();
     });
